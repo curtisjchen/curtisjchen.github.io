19a84cbda08ccc09684602b8d9a668ce592f19d8
curtisjchen <112734399+curtisjchen@users.noreply.github.com>
2026-03-03 03:20:45 +0000

    Deploying to gh-pages from @ curtisjchen/curtisjchen.github.io@eee9ca69a5f9adbf1cb54016e95b86d21f520be2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of projects I have done over time.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
